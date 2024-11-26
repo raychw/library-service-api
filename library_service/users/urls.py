0a5d37f98f56d4e6a575a07181dcf2f34f4a0380
raychw <bladeryz@gmail.com>
2024-11-26 21:32:06 +0200

    initialized urls for users service

---
+++ library_service/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", ManageUserView.as_view(), name="profile"),
+]
