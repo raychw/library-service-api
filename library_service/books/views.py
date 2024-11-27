32637feba1c4bdf365c4730cade747cffe13b44c
raychw <bladeryz@gmail.com>
2024-11-27 12:26:43 +0200

    fixed permission_classes

--- library_service/books/views.py
+++ library_service/books/views.py
@@ -7,7 +7,7 @@ from books.serializers import BookSerializer, BookListSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
-    permission_classes = IsAdminOrReadOnly
+    permission_classes = [IsAdminOrReadOnly]
 
     def get_serializer_class(self):
         if self.action == "list":
