d8623720ec3965059392ef2a140340b0db0360f5
raychw <bladeryz@gmail.com>
2024-11-29 14:01:47 +0200

    initialized borrowing model

--- library_service/borrowings/models.py
+++ library_service/borrowings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+from users.models import User
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
