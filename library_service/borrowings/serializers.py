d850a65c4491e1f3cd1a60b1669560b5b4a3b439
raychw <bladeryz@gmail.com>
2024-12-03 21:59:29 +0200

    implemented regular borrowing serializer and fixed borrowing list serializer

--- library_service/borrowings/serializers.py
+++ library_service/borrowings/serializers.py
@@ -4,8 +4,21 @@ from borrowings.models import Borrowing
 from books.serializers import BookSerializer
 
 
+class BorrowingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        )
+
+
 class BorrowingListSerializer(serializers.ModelSerializer):
-    book_title = serializers.CharField(
+    book = serializers.CharField(
         source="book.title", read_only=True
     )
 
