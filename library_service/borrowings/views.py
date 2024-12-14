1bbd315e865b930bfb197731e92961c1aa5677a1
raychw <bladeryz@gmail.com>
2024-12-14 21:34:21 +0200

    created filtering for borrowing endpoints, so now non-admin users can only see their own borrowings, borrowings are available only for authenticated users, and is_active and user_id parameters

--- library_service/borrowings/views.py
+++ library_service/borrowings/views.py
@@ -1,4 +1,7 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from borrowings.models import Borrowing
 from borrowings.serializers import (
@@ -10,7 +13,10 @@ from borrowings.serializers import (
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
-    queryset = Borrowing.objects.all()
+    queryset = Borrowing.objects.select_related("book", "user")
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ["is_active"]
+    ordering_fields = ["borrow_date", "expected_return_date"]
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -20,6 +26,30 @@ class BorrowingViewSet(viewsets.ModelViewSet):
         if self.action == "create":
             return BorrowingCreateSerializer
         return BorrowingSerializer
+
+    def get_permissions(self):
+        return [IsAuthenticated()]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        queryset = Borrowing.objects.select_related("book", "user")
+
+        if not user.is_staff:
+            queryset = queryset.filter(user=user)
+
+        is_active = self.request.query_params.get("is_active")
+        if is_active is not None:
+            if is_active.lower() == "true":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+            elif is_active.lower() == "false":
+                queryset = queryset.filter(actual_return_date__isnull=False)
+
+        user_id = self.request.query_params.get("user_id")
+        if user.is_staff and user_id:
+            queryset = queryset.filter(user_id=user_id)
+
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
