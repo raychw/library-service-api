84cb08d3e131f193d5159ae2bbd8ebbcee78bef1
raychw <bladeryz@gmail.com>
2024-12-26 23:39:36 +0200

    implemented payment viewset and limited user so they can only see their own payments

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from payments.models import Payment
+from payments.serializers import (
+    PaymentSerializer,
+    PaymentListSerializer,
+    PaymentDetailSerializer,
+)
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.prefetch_related("borrowing")
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return PaymentListSerializer
+        if self.action == "retrieve":
+            return PaymentDetailSerializer
+        return PaymentSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+
+        queryset = Payment.objects.select_related("borrowing", "user")
+
+        if not user.is_staff:
+            queryset = queryset.filter(user=user)
