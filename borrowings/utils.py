d9bea1f35f21e58634cb3bc2d8fb58761e9f7c91
raychw <bladeryz@gmail.com>
2024-12-26 14:24:45 +0200

    implemented a function that checks overdue borrowings and sends a detailed telegram notifications about each such borrowing

--- borrowings/utils.py
+++ borrowings/utils.py
@@ -1,6 +1,9 @@
 import os
 import requests
+from datetime import date
 from dotenv import load_dotenv
+
+from borrowings.models import Borrowing
 
 load_dotenv()
 
@@ -19,3 +22,25 @@ def send_telegram_message(text: str) -> None:
         response.raise_for_status()
     except requests.exceptions.RequestException as error:
         print(f"Error sending message: {error}")
+
+def check_overdue_borrowings() -> None:
+    today = date.today()
+
+    overdue_borrowings = Borrowing.objects.filter(
+        expected_return_date__lte=today, actual_return_date__isnull=True,
+    )
+
+    if overdue_borrowings.exists():
+        for borrowing in overdue_borrowings:
+            user = borrowing.user
+            book = borrowing.book
+
+            text = (
+                f"Overdue borrowing!\n\n"
+                f"User: {user.first_name} {user.last_name}\n"
+                f"Book: \"{book.title}\"\n\n"
+                f"Expected return date: {borrowing.expected_return_date.strftime('%d/%m/%Y')}"
+            )
+            send_telegram_message(text)
+    else:
+        send_telegram_message("No borrowings overdue today!")
