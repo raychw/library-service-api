939d7b1bb8a2c2c1098973a20add0641804712f7
raychw <bladeryz@gmail.com>
2024-12-28 16:15:34 +0200

    updated payment model and added better naming to amount field

--- payments/models.py
+++ payments/models.py
@@ -17,4 +17,4 @@ class Payment(models.Model):
     borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
     session_url = models.URLField()
     session_id = models.CharField(max_length=255)
-    money_to_pay = models.DecimalField(max_digits=6, decimal_places=2)
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
