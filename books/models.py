b61fadcca36c7233f680fa608b6ffa0c4e6b2a9b
raychw <bladeryz@gmail.com>
2025-01-02 18:35:21 +0200

    made validation in book model readable

--- books/models.py
+++ books/models.py
@@ -1,5 +1,5 @@
+from django.core.exceptions import ValidationError
 from django.db import models
-from django.core.validators import MinValueValidator
 from decimal import Decimal
 
 
@@ -12,14 +12,14 @@ class Book(models.Model):
     author = models.CharField(max_length=255)
     cover = models.CharField(max_length=4, choices=CoverType.choices)
     inventory = models.PositiveIntegerField(default=0)
-    daily_fee = models.DecimalField(
-        max_digits=6,
-        decimal_places=2,
-        validators=[MinValueValidator(Decimal(0.10))],
-    )
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2,)
 
     class Meta:
         ordering = ["title"]
 
     def __str__(self):
         return self.title
+
+    def clean(self):
+        if self.daily_fee < Decimal("0.10"):
+            raise ValidationError("Daily fee must be at least 0.10")
